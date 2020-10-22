5b9fbd448cb5159a7fd2e5861a87a2bf7aa759da
brightokike@gmail.com <brightokike@gmail.com>
2020-10-22 19:28:46 +0300

    added some css styling to the selectcolumn page

--- app/javascript/components/dataset/SelectColumns.js
+++ app/javascript/components/dataset/SelectColumns.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import { useSelector } from 'react-redux'
 
+
 const Container = styled.div`
 	max-width: 718px;
 	height: 500px;
@@ -12,28 +13,48 @@ const Container = styled.div`
   color: #2c2c2c;
   outline: none;
 	box-sizing: border-box;
+	
+	label {
+		font-size: 16px;
+	}
+	input {
+		width: 22px;
+		height: 22px;
+		margin: 10px;
+	}
+	input:hover {
+		cursor: pointer;
+		background-color: green;
+	}
 `;
 
 const FirstDiv = styled.div`
-
+	color: black;
 `;
 
 const LastDiv = styled.div`
-
+	color: black;
 `;
 
 const SelectColumns = () => {
 	const file = useSelector(state => state.file)
+	const keys = Object.keys(file.payload.data[0])
+	
+	const columnOptions = keys.map((header, i) => 
+		<label className="checkbox" key={i}>
+			<input type="checkbox" defaultChecked="checked" value={header} />
+			{header} <br />
+		</label>
+	)
+	
+
 	return (
-		
 		<Container>
-			{console.log(file)}
-			<FirstDiv>
-					Exclude columns by untoggling the checkbox
-			</FirstDiv>
-			<LastDiv>
+				{console.log(keys)}
+				Exclude Columns <br />
+				{	columnOptions }
 				Choose included columns to uniquely assign to ID, Name, and Timestamp
-			</LastDiv>
+	
 		</Container>
 	)
 }
